b41f4652447ccff073251cc68c2f359431114954
Irina Dumitrascu <me@dira.ro>
2010-05-01 16:45:09 -0300

    Explained the science

--- src/new.js
+++ src/new.js
@@ -1,58 +1,41 @@
+// Hypothesis:
+// - in the DOM, the map is in #map
+// - the markers have the properties "infoWindow" and "latlng", and they are 
+// no more than 3 levels deep compared to window.gApplication.getMap()
+
 function is_object(elem) {
   return (elem != null) && (typeof(elem) == 'object');
 }
 
 function has_property(container, property) {
-  return (typeof(container[property]) != 'undefined');
+  return typeof(container[property]) != 'undefined';
 }
 
 function is_marker(container) {
   return has_property(container, 'infoWindow') && has_property(container, 'latlng');
 }
 
+// Recursively explore the entire object space, looking for markers
 function explore(container, history) {
-  if (is_marker(container) && history.toString().indexOf(',0') > -1) {
+  if (is_marker(container) && history.toString().indexOf(',0') > -1) { // the marker should be in an array
     throw(history);
   }
-  if (history.length > max_level) return;
+  if (history.length < max_level) {
+    for (var property in container) {
+      if (!is_object(container[property])) continue;
+      if (has_property(container[property], 'parentNode')) continue; // don't look into DOM elements
 
-  for (property in container) {
-    if (!is_object(container[property])) continue;
-    if (has_property(container, 'parentNode')) continue;
-
-    explore(container[property], history.concat([property]));
-  }
-}
-
-map = window['gApplication'].getMap();
-max_level = 3;
-markers = [];
-
-try {
-  explore(map, []);
-} catch(stack) {
-  // stack has the form: [property, property, "0", property, property]
-  // before the index is the markers' container, after the index the properties to access inside the marker
-  container = map;
-  for (i = 0; i < stack.length; i++) {
-    if (stack[i] == '0') break;
-    container = container[stack[i]];
-  }
-  for (m in container) {
-    marker = container[m];
-    for (j = i+1; j < stack.length; j++) {
-      marker = marker[stack[j]];
+      explore(container[property], history.concat([property]));
     }
-    markers.push(marker);
   }
 }
 
 function get_color(marker) {
-  for (property in marker) {
+  for (var property in marker) {
     if (marker[property].indexOf && marker[property].indexOf('.png') > -1) {
-      candidate = marker[property];
-      if (candidate.match(/^http/)) {
-        color = candidate.match('/[^/.]*.png')[0];
+      var color = marker[property];
+      if (color.match(/^http/)) {
+        color = color.match('/[^/.]*.png')[0];
         color = color.substring(1, color.length - 4); // get the color name from the image
         color = color.replace('-dot', '');
         if (color == 'pink') color = '0xCE579A';
@@ -64,21 +47,48 @@ function get_color(marker) {
   return '';
 }
 
+function get_zoom() {
+  /* Hic sunt magiks! :D
+     r (map_width/longitude_span) is constant within a zoom level and diminuates exponentially as we zoom in,
+     with powers of 2, hence the mighty log2()
+     For level 3, 1/r = 0.175. I like level 3 because, for not extravagant resolutions, there is no map part shown twice,
+     so longitude_span is actually meaningful */
+  var r = document.getElementById('map').offsetWidth / map.getBounds().toSpan().x;
+  return 3 + Math.round(Math.LOG2E * Math.log(0.175 * r));
+}
+
+map = window.gApplication.getMap();
+max_level = 3;
+markers = [];
+
+try {
+  explore(map, []);
+} catch(stack) {
+  // yuppie, found markers
+  // stack has the form: [property, property, "0", property, property]
+  container = map;
+  for (var i = 0; i < stack.length; i++) {
+    if (stack[i] == '0') break;
+    container = container[stack[i]];
+  }
+  for (var m in container) {
+    var marker = container[m];
+    for (var j = i+1; j < stack.length; j++) {
+      marker = marker[stack[j]];
+    }
+    markers.push(marker);
+  }
+}
+
 size = '350x450';
+// todo: maptype {roadmap,sattelite,hybrid,terrain}
+url = 'http://maps.google.com/maps/api/staticmap?zoom=' + get_zoom() + '&size=' + size + '&center=' + map.getCenter().toUrlValue() + '&maptype=roadmap&sensor=false';
 
-center = map.getCenter().toUrlValue();
-zoom = 3 + Math.round(Math.LOG2E * Math.log(0.175 * window.outerWidth / map.getBounds().toSpan().x));
-//roadmap,sattelite,hybrid,terrain
-
-url = 'http://maps.google.com/maps/api/staticmap?zoom=' + zoom + '&size=' + size + '&center=' + center + '&maptype=roadmap&sensor=false';
-
-for (i=0; i < markers.length; i++) {
-  marker = markers[i];
+for (var i = 0; i < markers.length; i++) {
+  var marker = markers[i];
   try {
-    latLng = marker['latlng'];
-    color = get_color(marker);
-
-    url += '&markers=color:' + color + '|' + latLng.lat.toFixed(2) + ',' + latLng.lng.toFixed(2);
+    var latLng = marker['latlng'];
+    url += '&markers=color:' + get_color(marker) + '|' + latLng.lat.toFixed(2) + ',' + latLng.lng.toFixed(2);
   }catch(err){}
 }
 window.open(url, '_blank');
