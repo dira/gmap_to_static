77dd797aee196e9085bbe48146526604cbb514f7
Irina Dumitrascu <me@dira.ro>
2010-06-08 17:25:08 -0300

    Restructured in a namespace + Group markers by color in the URL

--- gmap_to_static.js
+++ gmap_to_static.js
@@ -1,4 +1,4 @@
-(function() {
+Dira = {1:1
   // Hypothesis:
   // - in the DOM, the map is in #map
   // - markers, paths and shapes can be identified by their "quack"
@@ -7,52 +7,52 @@
   //
   // Exploration
   //
-  function is_object(elem) {
+  ,is_object: function(elem) {
     return (elem != null) && (typeof(elem) == 'object');
   }
 
-  function has_property(container, property) {
+  ,has_property: function(container, property) {
     return typeof(container[property]) != 'undefined';
   }
 
-  function is_marker(container) {
-    return has_property(container, 'infoWindow') && has_property(container, 'latlng');
+  ,is_marker: function(container) {
+    return this.has_property(container, 'infoWindow') && this.has_property(container, 'latlng');
   }
 
-  function is_path(container) {
-    return has_property(container, "getVertex") && has_property(container, 'name');
+  ,is_path: function(container) {
+    return this.has_property(container, "getVertex") && this.has_property(container, 'name');
   }
 
-  function is_shape(container) {
-    return has_property(container, "getVertex") && !has_property(container, 'name');
+  ,is_shape: function(container) {
+    return this.has_property(container, "getVertex") && !this.has_property(container, 'name');
   }
 
-  function is_shape_bgrd(container) {
-    return has_property(container, 'color') && has_property(container, 'fill');
+  ,is_shape_bgrd: function(container) {
+    return this.has_property(container, 'color') && this.has_property(container, 'fill');
   }
 
-  function result_must_be_array(quack) {
-    return quack != is_shape_bgrd;
+  ,result_must_be_array: function(quack) {
+    return quack != this.is_shape_bgrd;
   }
 
   // Recursively explore the entire object space, until finding the needed object
-  function search_by_quack(container, quack, max_level, path) {
-    if (quack(container)) {
-      if ( !result_must_be_array(quack) || path.toString().indexOf(',0') > -1) {
+  ,search_by_quack: function(container, quack, max_level, path) {
+    if (quack.apply(Dira, [container])) {
+      if ( !this.result_must_be_array(quack) || path.toString().indexOf(',0') > -1) {
         throw({ type: 'GMap-to-static', path: path });
       }
     }
     if (path.length < max_level) {
       for (var property in container) {
-        if (!is_object(container[property])) continue;
-        if (has_property(container[property], 'parentNode')) continue; // don't look into DOM elements
+        if (!this.is_object(container[property])) continue;
+        if (this.has_property(container[property], 'parentNode')) continue; // don't look into DOM elements
 
-        search_by_quack(container[property], quack, max_level, path.concat([property]));
+        this.search_by_quack(container[property], quack, max_level, path.concat([property]));
       }
     }
   }
 
-  function down(containers, property) {
+  ,down: function(containers, property) {
     var result = [];
     for (var c in containers) {
       if (Number(property) == property) {
@@ -66,16 +66,18 @@
     return result.filter(function(e){return e != null;});
   }
 
-  function search_for(quack, max_level) {
+  ,search_for: function(quack, max_level) {
     var results = [];
     try {
-      search_by_quack(map, quack, max_level, []);
+      this.search_by_quack(this.map, quack, max_level, []);
     } catch(e) {
       if (e.type && e.type == 'GMap-to-static') {
-        var results = [map];
+        var results = [this.map];
         for (var i = 0; i < e.path.length; i++) {
-          results = down(results, e.path[i]);
+          results = this.down(results, e.path[i]);
         }
+      } else {
+        throw(e);
       }
     }
     return results;
@@ -84,24 +86,26 @@
   //
   // Data extraction
   //
-  function extract_info() {
-    var markers = search_for(is_marker, 3);
-    var paths = search_for(is_path, 4);
-    var shapes = search_for(is_shape, 4);
+  ,extract_info: function() {
+    var markers = this.search_for(this.is_marker, 3);
+    var paths = this.search_for(this.is_path, 4);
+    var shapes = this.search_for(this.is_shape, 4);
 
-    var size = [Math.min(640, Math.round(map_element.offsetWidth)),
-                Math.min(640, Math.round(map_element.offsetHeight))];
-    var zoom = get_zoom();
+    var size = [Math.min(640, Math.round(this.map_element.offsetWidth)),
+                Math.min(640, Math.round(this.map_element.offsetHeight))];
+    var zoom = this.get_zoom();
               '&zoom=' + zoom + '&size=' + size.join('x') +
-              '&center=' + map.getCenter().toUrlValue() + '&sensor=false';
+              '&center=' + this.map.getCenter().toUrlValue() + '&sensor=false';
 
-    var bounds = get_actual_bounds(size);
-    markers = markers.filter(function(marker){ return between(bounds, marker['latlng']); }).map(extract_marker);
+    var bounds = this.get_actual_bounds(size);
+    var _this = this;
+    markers = markers.filter(function(marker){ return _this.between(bounds, marker['latlng']); }).
+      map(function(marker) { return _this.extract_marker(marker) } );
 
-    paths = paths.map(function(path){ return extract_path(path, bounds, false); }).
+    paths = paths.map(function(path){ return _this.extract_path(path, bounds, false); }).
       filter(function(e){return e != null;});
 
-    shapes = shapes.map(function(path){ return extract_path(path, bounds, true); }).
+    shapes = shapes.map(function(path){ return _this.extract_path(path, bounds, true); }).
       filter(function(e){return e != null;});
 
 
@@ -114,33 +118,33 @@
     };
   }
 
-  function get_zoom() {
+  ,get_zoom: function() {
     /* Hic sunt magiks! :D
        r (map_width/longitude_span) is constant within a zoom level and diminuates exponentially as we zoom in,
        with powers of 2, hence the mighty log2()
        For level 3, 1/r = 0.175. I like level 3 because, for not extravagant resolutions, there is no map part shown twice,
        so longitude_span is actually meaningful */
-    var r = map_element.offsetWidth / map.getBounds().toSpan().x;
+    var r = this.map_element.offsetWidth / this.map.getBounds().toSpan().x;
     return 3 + Math.round(Math.LOG2E * Math.log(0.175 * r));
   }
 
-  function get_actual_bounds(size) {
-    var top_left = map.fromLatLngToDivPixel(map.getCenter());
+  ,get_actual_bounds: function(size) {
+    var top_left = this.map.fromLatLngToDivPixel(this.map.getCenter());
     top_left.x -= Math.round(size[0] / 2); top_left.y -= Math.round(size[1] / 2);
-    top_left = map.fromDivPixelToLatLng(top_left);
-    var bottom_right = map.fromLatLngToDivPixel(map.getCenter());
+    top_left = this.map.fromDivPixelToLatLng(top_left);
+    var bottom_right = this.map.fromLatLngToDivPixel(this.map.getCenter());
     bottom_right.x += Math.round(size[0] / 2); bottom_right.y += Math.round(size[1] / 2);
-    bottom_right = map.fromDivPixelToLatLng(bottom_right);
+    bottom_right = this.map.fromDivPixelToLatLng(bottom_right);
     return { nw: top_left, se: bottom_right };
   }
 
-  function extract_marker(marker) {
-    var info = get_color(marker);
+  ,extract_marker: function(marker) {
+    var info = this.get_color(marker);
     info.latlng = marker['latlng'];
     return info;
   }
 
-  function extract_path(path, bounds, is_shape) {
+  ,extract_path: function(path, bounds, is_shape) {
     var points = [];
     var n = path.getVertexCount();
     // for each node that is in the map area, add the predecesor and the succesor
@@ -148,7 +152,7 @@
     var to_add = [];
     for (var i = 0; i < n; i++) {
       var v = path.getVertex(i);
-      if (between(bounds, {lat: v.lat(), lng: v.lng()})) {
+      if (this.between(bounds, {lat: v.lat(), lng: v.lng()})) {
         to_add.push(i, i+1, i-1);
       }
     }
@@ -158,21 +162,21 @@
       points.push({ lat: v.lat(), lng: v.lng() });
     }
     if (points.length < 2) return null;
-    var info = { color: encode_hex(path.color) };
+    var info = { color: this.encode_hex(path.color) };
     if (is_shape) {
       try {
-        search_by_quack(path, is_shape_bgrd, 3, []);
+        this.search_by_quack(path, this.is_shape_bgrd, 3, []);
       } catch(e) {
         var fill = path;
         for (var i in e.path) fill = fill[e.path[i]]; // TODO why not down
-        info.fillcolor = encode_hex(fill.fill) + Math.round(fill.opacity * 255).toString(16);
+        info.fillcolor = this.encode_hex(fill.fill) + Math.round(fill.opacity * 255).toString(16);
       }
     }
     info.points = points;
     return info;
   }
 
-  function get_color(marker) {
+  ,get_color: function(marker) {
     for (var property in marker) {
       if (marker[property].indexOf && marker[property].indexOf('.png') > -1) {
         var color = marker[property];
@@ -193,69 +197,86 @@
         }
       }
     }
-    return '';
+    return {};
   }
 
-  function between(bounds, point) {
+  ,between: function(bounds, point) {
     return  point.lat <= bounds.nw.lat() && point.lng >= bounds.nw.lng() &&
             point.lat >= bounds.se.lat() && point.lng <= bounds.se.lng();
   }
 
-  function encode_hex(hex) {
+  ,encode_hex: function(hex) {
     return hex.replace('#', '0x');
   }
 
   //
   // URL construction
   //
-  function get_url(info) {
+  ,get_url: function(info) {
     var url = 'http://maps.google.com/maps/api/staticmap?maptype=roadmap' +
               '&zoom=' + info.zoom + '&size=' + info.size.join('x') +
-              '&center=' + map.getCenter().toUrlValue() + '&sensor=false';
+              '&center=' + this.map.getCenter().toUrlValue() + '&sensor=false';
 
+    var _this = this;
     if (info.markers.length > 0) {
-      url += '&' + info.markers.map(function(marker) {
-        return 'markers=color:' + marker.color + '|' + encode_latlng(marker.latlng);
-      }).join('&');
+      var by_color = this.group_by_color(info.markers);
+      for (var color in by_color) {
+        url += '&markers=color:' + color + '|' + by_color[color].map(function(marker) {
+          return _this.encode_latlng(marker.latlng);
+        }).join('|');
+      }
     }
 
     if (info.shapes.length > 0) {
       url += '&' + info.shapes.map(function(shape){
-        return encode_path(shape, true);
+        return _this.encode_path(shape, true);
       }).join("&");
     }
 
     if (info.paths.length > 0) {
       url += '&' + info.paths.map(function(path){
-        return encode_path(path, false);
+        return _this.encode_path(path, false);
       }).join("&");
     }
 
     return url;
   }
 
-  function encode_path(path, is_shape) {
+  ,group_by_color: function(markers) {
+    var result = {};
+    for (i in markers) {
+      var marker = markers[i];
+      if (result[marker.color] == null) {
+        result[marker.color] = [];
+      }
+      result[marker.color].push(marker);
+    }
+    return result;
+  }
+
+  ,encode_path: function(path, is_shape) {
     url = 'path=';
     if (is_shape) {
       url += 'fillcolor:' + path.fillcolor + '|';
     }
     url += 'color:' + path.color + '|';
-    return url + path.points.map(encode_latlng).join("|");
+    var _this = this;
+    return url + path.points.map(function(point) { return _this.encode_latlng(point) }).join("|");
   }
 
-  function encode_latlng(latlng) {
-    return latlng.lat.toFixed(precision) + ',' + latlng.lng.toFixed(precision);
+  ,encode_latlng: function(latlng) {
+    return latlng.lat.toFixed(this.precision) + ',' + latlng.lng.toFixed(this.precision);
   }
 
-  //
-  // Let's roll!
-  //
-  var map = window.gApplication.getMap();
-  var map_element = document.getElementById('map');
-  var precision = 3;
-  var info = extract_info();
-  var url = get_url(info);
 
-  var opened = window.open(url);
-  if (opened == null) prompt("The address of the static map is:", url);
-})();
+  ,map : function() { return window.gApplication.getMap()}()
+  ,map_element : document.getElementById('map')
+  ,precision : 3
+
+  ,main: function() {
+    var url = this.get_url(this.extract_info());
+    var opened = window.open(url);
+    if (opened == null) prompt("The address of the static map is:", url);
+  }
+}
+Dira.main();
